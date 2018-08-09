58567e879a0ea30829031ad932b3409608f15d05
omprakashsridharan <omprakashsridharan@outlook.com>
2018-08-09 16:40:10 +0530

    Second commit

--- src/Solution.java
+++ src/Solution.java
@@ -1,5 +1,7 @@
+
+
 public class Solution {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
     }
 }
--- src/SowpodsUtil.java
+++ src/SowpodsUtil.java
@@ -1,3 +1,46 @@
-public class SowpodsUtil {
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 
+public class SowpodsUtil {
+    public static HashMap<Integer, ArrayList<String>> anagramMap ;
+
+    public static void readFromFile() throws IOException {
+        anagramMap = new HashMap<>();
+        String fileName = "src/sowpods.txt";
+        String line;
+        FileReader fileReader =
+                new FileReader(fileName);
+        BufferedReader bufferedReader =
+                new BufferedReader(fileReader);
+        while((line = bufferedReader.readLine()) != null ) {
+            String sortedString = sortString(line);
+            if(anagramMap.get(line.length()) == null){
+                ArrayList arrayList = new ArrayList();
+                arrayList.add(sortedString);
+                anagramMap.put(line.length(),arrayList);
+            }else {
+                ArrayList arrayList = anagramMap.get(line.length());
+                if(!arrayList.contains(sortedString)){
+                    arrayList.add(sortedString);
+                    anagramMap.put(line.length(),arrayList);
+                }
+            }
+        }
+        bufferedReader.close();
+    }
+
+    public static HashMap getAnagramMap() throws IOException {
+        readFromFile();
+        return anagramMap;
+    }
+
+    private static String sortString(String anagram) {
+        char tempArray[] = anagram.toCharArray();
+        Arrays.sort(tempArray);
+        return new String(tempArray);
+    }
 }
